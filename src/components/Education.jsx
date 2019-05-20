f7432428eac800e21506f3af4a668dfe1a072052
georgerangelcode <georgerangelcode@gmail.com>
2019-05-20 12:49:42 -0500

    Agregando Custom Styles

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import H2Styled from "../styles/H2Styled.jsx";
+import H3Styled from "../styles/H3Styled.jsx";
+import PStyled from "../styles/PStyled.jsx";
 
 const Education = props => (
   <div className="Education">
+    <H2Styled name="Education" />
     <div className="Education-container">
       {props.data.map((edu, index) => (
         <div className="Education-item" key={`Edu-${index}`}>
-          <h3>
+          <H3Styled>
             {edu.degree} {edu.institution}
             <span>
               {edu.startDate} - {edu.endDate}
             </span>
-          </h3>
-          <p>Aprendi Mucho</p>
+          </H3Styled>
+          <PStyled name={edu.description} />
         </div>
       ))}
     </div>
