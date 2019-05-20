3f2c7f989a2b317981579c6453e6be73dbef57b8
georgerangelcode <georgerangelcode@gmail.com>
2019-05-20 12:53:53 -0500

    Añadiendo estilos custom a componente Education y Certificates

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import H2Styled from "../styles/H2Styled.jsx";
+import H3Styled from "../styles/H3Styled.jsx";
+import PStyled from "../styles/PStyled.jsx";
 
 const Certificates = props => (
   <div className="Certificates">
+    <H2Styled name="Certificates" />
     <div className="Certificates-container">
       {props.data.map((cer, index) => (
         <div className="Certificates-item" key={`Cer-${index}`}>
-          <h3>
+          <H3Styled>
             {cer.name} @ {cer.institution}
             <span>{cer.date}</span>
-          </h3>
-          <p>{cer.description}</p>
+          </H3Styled>
+          <PStyled name={cer.description} />
         </div>
       ))}
     </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import H2Styled from "../styles/H2Styled.jsx";
+import H3Styled from "../styles/H3Styled.jsx";
+import PStyled from "../styles/PStyled.jsx";
 
 const Experience = props => (
   <div className="Experience">
+    <H2Styled name="Experience" />
     <div className="Experience-container">
       {props.data.map((exp, index) => (
         <div className="Experience-item" key={`Exp-${index}`}>
-          <h3>
+          <H3Styled>
             {exp.jobTitle} @ {exp.company}
             <span>
               {exp.startDate} - {exp.endDate}
             </span>
-          </h3>
-          <p>{exp.jobDescription}</p>
+          </H3Styled>
+          <PStyled name={exp.jobDescription} />
         </div>
       ))}
     </div>
